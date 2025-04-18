79be4a30df6873f91829db3a38f8995baf79a4b3
Shanshuimei <1316317715@qq.com>
2025-04-18 21:54:17 +0800

    chore: 更新Vite配置中的base路径
    将base路径从'/New_Hamlet/'更改为'/New_Hamlet_Beyond_the_Five_Acts/'，以匹配项目的新命名

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/New_Hamlet/',
+  base: '/New_Hamlet_Beyond_the_Five_Acts/',
   // 添加代理配置
   server: {
     proxy: {
